6b554339fc50db50b46176e02ae4c58eb8189dfc
JonaszOlk <jonaszolkowski@gmail.com>
2023-10-31 11:52:44 +0100

    first commit

---
+++ Hello World2.py
@@ -0,0 +1,75 @@
+print('hello, world2!')
+#first_name = 'Jonasz'
+#last_name = 'Olkowski'
+#full_name = first_name + " " + last_name
+#print('Hello ' + first_name)
+#print(type(first_name))
+#print('hello ' + full_name)
+#age = 23
+#age = age + 1
+#print(age)
+#print(type(age))
+#print('Your age is: ' + (str(age)))
+#height = 176.6
+#print('Your height is ' + (str(height)) + 'cm')
+#print(type(height))
+#human = False
+#print(human)
+#print(type(human))
+#print('Are you a human? ' + (str(human)))
+# name = 'Jonasz'
+# age = 23
+# attractive = True
+# name, age, attractive = 'Jonasz', 23, True
+# print(name)
+# print(age)
+# print(attractive)
+
+#spongebob = 30
+#patrick = 30
+#sandy = 30
+#squidward = 30
+
+# spongebob = patrick = sandy = squidward = 30
+
+# print(spongebob)
+# print(patrick)
+# print(sandy)
+# print(squidward)
+
+#name = 'Jonasz'
+#name1 = 'jonasz'
+# print(name)
+# print(len(name))
+# print(name.find("a"))
+#print(name1.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count('o'))
+# print(name.replace('o', 'p'))
+# print(name*3)
+
+#x = 1
+#y = 2.0
+#z = '3'
+
+#print('X is ' + str(x))
+#print('Y is ' + str(y))
+#print("Z is " + z*3)
+
+#name = input("What is your name?: ")
+#age = int(input('How old are you?: '))
+#age = age + 1
+#height = float(input('How tall are you?: '))
+#print('Hello ' + name)
+#print('Your age is: ' +str(age))
+#print('You are ' + str(height) +"cm tall")
+
+import math 
+pi = 3.14
+print(round(pi))
+print(math.ceil(pi))
+
+
